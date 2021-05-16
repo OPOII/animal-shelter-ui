773010574d33a243e5459c7869495d707fadcb20
OPOII <41926039+OPOII@users.noreply.github.com>
2021-05-16 18:54:15 -0500

    Update publish.js

--- test/contract/config/publish.js
+++ test/contract/config/publish.js
@@ -6,7 +6,7 @@ const opts = {
     pactBroker: process.env.PACT_BROKER_BASE_URL,
     pactBrokerToken: process.env.PACT_BROKER_TOKEN,
     consumerVersion: process.env.PACT_CONSUMER_VERSION,
-    pactFilesOrDirs: ['D:/animal-shelter-ui/test/contract/pacts/animalshelterfront-animalshelterback.json']
+    pactFilesOrDirs: ['./test/contract/pacts/animalshelterfront-animalshelterback.json']
 };
 
 new Publisher(opts).publishPacts();
